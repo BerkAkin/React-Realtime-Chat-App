e958dc35772c00e9351a5029a572ca9a78fcc732
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-12-21 16:22:27 +0300

    feat: send message form is now implemented

--- chat/client/src/components/ChatForm.js
+++ chat/client/src/components/ChatForm.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import styles from './styles.module.css'
+import { useState } from 'react'
 
 function ChatForm() {
+  const [message,setMessage] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage('');
+  }
   return (
-    <div>ChatForm</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input value={message} onChange={(e)=>setMessage(e.target.value)} type='text' className={styles.textInput}></input>
+      </form>
+    </div>
   )
 }
 

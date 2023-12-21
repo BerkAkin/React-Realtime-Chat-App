f119a72c1701d4b7d5d18f56b9ca848cbd03032b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-12-21 17:08:20 +0300

    feat: messages can now be right aligned from default me user and messages can be sent now

--- chat/client/src/components/ChatForm.js
+++ chat/client/src/components/ChatForm.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import styles from './styles.module.css'
 import { useState } from 'react'
+import {sendMessage} from '../socketApi'
+import { useChat } from '../context/ChatContext'
 
 function ChatForm() {
   const [message,setMessage] = useState('')
+
+  const {setMessages} = useChat();
+
   const handleSubmit = (e) => {
+
     e.preventDefault();
+
+    setMessages((prev)=>[...prev,{message, fromMe:true}])
+
+    sendMessage(message)
+
     setMessage('');
+    
   }
   return (
     <div>

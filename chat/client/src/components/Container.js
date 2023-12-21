2a4bacb34cdf50bd0f1c4e23cf518e1df639ad68
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-12-21 17:09:21 +0300

    feat: new messages can now be shown on chat history

--- chat/client/src/components/Container.js
+++ chat/client/src/components/Container.js
@@ -1,12 +1,28 @@
 import {React,useEffect} from 'react'
 import ChatList from './ChatList'
 import ChatForm from './ChatForm'
-import {init} from '../socketApi'
+import {init, subscribeChat, subscribeInitialMessages} from '../socketApi'
+import {useChat} from '../context/ChatContext'
 
 function Container() {
+  const {setMessages}=useChat();
+
   useEffect(()=>{
+
     init();
+
+    subscribeChat((message)=>{
+      setMessages((prev)=>[...prev,{message}])
+    });
+
+    subscribeInitialMessages((messages)=>{
+      setMessages(messages)
+      console.log(messages);
+    })
+
   },[])
+
+
   return (
     <div>
         <ChatList/>
